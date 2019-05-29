9191492d5e0f3d4ba957131833631fc7b7d29768
edugtz <eduardo.gutierrez.2325@gmail.com>
2019-05-29 07:15:31 -0500

    Initial app structure

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.scss";
+
+const App = () => {
+  return (
+    <div className="App-text-color">
+        <h1>React App</h1>
+    </div>
+  );
+};
+
+export default App;
